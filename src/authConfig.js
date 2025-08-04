158ba1eef3a3ffec008a8b32d1d353e96eaed02f
zura72 <wiqolby@gmail.com>
2025-08-04 14:02:24 +0700

    fix: update redirectUri ke domain publik

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,10 +1,11 @@
-// src/authConfig.js
+const isLocalhost = window.location.hostname === "localhost";
+
 export function getMsalConfig(persistent = false) {
   return {
     auth: {
-      clientId: "f536a53d-8a16-45cf-9acf-d8c77212b605",
+      clientId: "f536a53d-8a16-45cf-9acf-d8c77212b605", // GANTI DENGAN PUNYAMU!
       authority: "https://login.microsoftonline.com/94526da5-8783-4516-9eb7-8c58bbf66a2d",
-      redirectUri: "http://localhost:8080/login",
+      redirectUri: "https://waskita-karya-infrastruktur-it-dashboard.site/login",
     },
     cache: {
       cacheLocation: persistent ? "localStorage" : "sessionStorage",
